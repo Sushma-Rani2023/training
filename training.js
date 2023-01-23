57276b31953b0cc392fc76a9e7ed642c524e8919
Sushma-Rani2023 <86097622+Sushma-Rani2023@users.noreply.github.com>
2023-01-23 17:11:07 +0530

    Add files via upload

---
+++ training.js
@@ -0,0 +1,47 @@
+// Concept of variables in js
+var x=5;
+let y=6;
+console.log(x,y);
+console.log(y)
+
+var x=5;
+function add(data){
+    
+    return x
+}
+
+function ne(){
+    let x;
+    let y=6;
+    y+=1;
+    return y
+    
+}
+// Arrow function 
+hii=(x)=>x+1
+console.log(hii(x))
+
+// Ternary operator in javascript
+console.log(5==6?'Hi':'No')
+console.log(5=='5'?'Hi':'No')
+console.log(5==='5'?'Hi':'No')
+
+// Concept of lopps in Javascript
+
+var arr=[1,'Sushma',true,0]
+for (var i in arr){
+    console.log(i,arr[i]);
+
+}
+
+
+var arr2=[1,'Sushma',true,0];
+for (i of arr2)
+{
+    console.log(i)
+}
+
+
+console.log(y)
+console.log(ne())
+console.log(add(x))
